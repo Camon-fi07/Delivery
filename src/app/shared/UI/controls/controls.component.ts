e1dee1921dcd54d15d7e5f4cbcce591ad8e01109
Camon-fi07 <422nikitos244@mail.ru>
2024-02-01 00:45:19 +0700

    Сверстал компонент выбора и написал изначальную логику 🎛️

---
+++ src/app/shared/UI/controls/controls.component.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { TextType } from 'shared/UI/text/text.types';
+import { TextComponent } from 'shared/UI/text/text.component';
+
+@Component({
+  selector: 'controls',
+  standalone: true,
+  imports: [CommonModule, TextComponent],
+  templateUrl: './controls.component.html',
+  styleUrl: './controls.component.scss',
+})
+export class ControlsComponent implements OnInit {
+  @Input({ required: true }) values!: string[];
+  @Output() emitChosenIndex = new EventEmitter<number>();
+  @Input() isMultiSelect = false;
+  @Input() withIcon = false;
+  @Input() isDisabled = false;
+
+  chosenIndexes = [0];
+  textValueType = TextType.VALUE;
+
+  handleChose(index: number) {
+    if (!this.isDisabled) this.emitChosenIndex.emit(index);
+
+    if (this.isMultiSelect) {
+      if (this.chosenIndexes.includes(index))
+        this.chosenIndexes = this.chosenIndexes.filter((value) => value !== index);
+      else this.chosenIndexes.push(index);
+    } else this.chosenIndexes[0] = index;
+  }
+
+  ngOnInit(): void {
+    if (this.isMultiSelect) this.withIcon = true;
+  }
+}
