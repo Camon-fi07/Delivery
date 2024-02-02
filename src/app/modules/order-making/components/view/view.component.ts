d62fe8c321067e605fd72f6f13128749acf39a11
Camon-fi07 <422nikitos244@mail.ru>
2024-02-02 23:24:04 +0700

    Пофиксил стили и добавл краткую информацию о доставке 🧭

--- src/app/modules/order-making/components/view/view.component.ts
+++ src/app/modules/order-making/components/view/view.component.ts
@@ -1,9 +1,12 @@
+import { ButtonStyles } from 'shared/UI/button/button.types';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { OrderInfoService } from 'modules/order-making/services/order-info.service';
 import { translateUserInfo, translateAddress } from 'modules/order-making/utils/mappers';
 import { CardData } from 'shared/UI/card/card.types';
 import { TextType } from 'shared/UI/text/text.types';
+import { DeliveryOption } from 'shared/types/Calc';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'view',
@@ -12,22 +15,30 @@ import { TextType } from 'shared/UI/text/text.types';
 })
 export class ViewComponent {
   TextType = TextType;
+  ButtonType = ButtonStyles;
   sender!: CardData[];
   receiver!: CardData[];
   senderAddress!: CardData[];
   receiverAddress!: CardData[];
+  option!: DeliveryOption;
 
   constructor(
     private orderInfoService: OrderInfoService,
     private router: Router,
+    private location: Location,
   ) {
     this.sender = translateUserInfo(orderInfoService.sender!);
     this.receiver = translateUserInfo(orderInfoService.receiver!);
     this.senderAddress = translateAddress(orderInfoService.senderAddress!);
     this.receiverAddress = translateAddress(orderInfoService.receiverAddress!);
+    this.option = orderInfoService.option!;
   }
 
   handleCardChange(url: string) {
     this.router.navigate([`order/${url}`]);
+  }
+
+  handleBack() {
+    this.location.back();
   }
 }
--- src/app/modules/order-making/services/order-info.service.ts
+++ src/app/modules/order-making/services/order-info.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { DeliveryOption } from 'shared/types/Calc';
+import { DeliveryOption, DeliveryType } from 'shared/types/Calc';
 import { Address } from 'shared/types/Point';
 import { Payer, UserDeliveryInfo } from 'shared/types/User';
 
 @Injectable()
 export class OrderInfoService {
-  option?: DeliveryOption;
+  option?: DeliveryOption = { days: 1, id: 'awf', name: 'fawf', price: 23, type: DeliveryType.DEFAULT };
   sender?: UserDeliveryInfo = { firstname: 'few', lastname: 'fawf', phone: '4234234234' };
   receiver?: UserDeliveryInfo = { firstname: 'few', lastname: 'fawf', phone: '4234234234' };
   senderAddress?: Address = { house: 'few', street: 'fawf', appartament: '4234234234' };
